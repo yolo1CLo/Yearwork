727dc9cb3f9239b60b501fb3187c72b1ee64b9bf
yolo1CLo <122444241+yolo1CLo@users.noreply.github.com>
2023-10-29 17:56:35 +0100

    Main.py

---
+++ Main.py
@@ -0,0 +1,10 @@
+from Calc import my_Calculator
+
+# Example Usage
+calc = my_Calculator("Calculatoooor")
+print(calc.introduce_my_self)
+print(calc.quadratic(1, -3, 2))
+print(calc.Newton(5, 4))
+print (calc.Speed (4, 8))
+print (calc.Kinetic_Energy (4,3,7))
+print (calc.Work (5,8,6))
