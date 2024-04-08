e11676d28415690a995ea50683328e4bca6274fd
AdelNehili <adel@LAPTOP-435R1L8O>
2024-04-08 10:27:38 +0200

    General Archi Modification

--- PyQT.py
+++ PyQT.py
@@ -1,58 +1,52 @@
 from PyQt5 import QtWidgets, uic
-from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
-class MyWindow (QMainWindow):
-    def __init__ (self,app):
-        super(MyWindow, self). __init__()
-        uic.loadUi('screen1.ui', self)
+class MyWindow(QMainWindow):
+    def __init__(self, stackWidget):
+        super(MyWindow, self).__init__()
+        uic.loadUi('screen1.ui', self)  # Load the screen.ui file parameters
+        self.stackWidget = stackWidget
+        self.pushButton.clicked.connect(self.gotoCalc)  # Assuming pushButton exists in screen1.ui
 
-        self.app = app
-        self.stackWidget = QtWidgets.QStackedWidget() # Create a single QStackedWidget
+    def gotoCalc(self):
+        self.stackWidget.setCurrentIndex(1)
 
-        self.pushButton.clicked.connect(self.gotocalc)
-        #self.pushButton_2.clicked.connect(self.gotocalc)
-
-
-    def gotocalc(self):
-        
-        self.stackWidget.setCurrentIndex(1) # Show mainwindow first
-        self.stackWidget.show() # Show the stackWidget
-
-class calc (QMainWindow):
-    def __init__ (self,app):
-        super(calc, self). __init__()
+class Calc(QMainWindow):
+    def __init__(self, stackWidget):
+        super(Calc, self).__init__()
         uic.loadUi('screen2.ui', self)
+        self.stackWidget = stackWidget
+        self.pushButton_1.clicked.connect(self.gotoQuadratic)  # Assuming pushButton_1 exists in screen2.ui
 
-        self.app = app
-        self.stackWidget = QtWidgets.QStackedWidget() # Create a single QStackedWidget
+    def gotoQuadratic(self):
+        self.stackWidget.setCurrentIndex(2)
 
-        self.pushButton_1.clicked.connect(self.goto_quadratic)
-   
-    def goto_quadratic(self):
-        self.stackWidget.setCurrentIndex(2) 
-        self.stackWidget.show()
-
-class quadratic (QMainWindow):
-    def __init__ (self):
-        super(quadratic, self). __init__()
+class Quadratic(QMainWindow):
+    def __init__(self, stackWidget):
+        super(Quadratic, self).__init__()
         uic.loadUi('quadratic.ui', self)
+        self.stackWidget = stackWidget
+        # Add any button connections here if needed
 
-def window():
-    app = QApplication(sys.argv)
+def main():
+    app = QApplication(sys.argv) #QWidget: Must construct a QApplication before a QWidget
+    stackWidget = QtWidgets.QStackedWidget()  # Centralized QStackedWidget
+
     
-    mainwindow = MyWindow(app)
-    screen2 = calc(app) # Assuming you want to initialize this at the start
-    screen3 = quadratic()
+    # Create screen instances and pass the shared QStackedWidget to them
+    main_window = MyWindow(stackWidget)
+    calc_window = Calc(stackWidget)
+    quadr_window = Quadratic(stackWidget)
 
-    mainwindow.stackWidget.addWidget(mainwindow) # Add mainwindow
-    mainwindow.stackWidget.addWidget(screen2) # Add screen2
-    mainwindow.stackWidget.addWidget(screen3) # Add screen3
+    # Add screens to the QStackedWidget
+    stackWidget.addWidget(main_window)
+    stackWidget.addWidget(calc_window)
+    stackWidget.addWidget(quadr_window)
 
-
-    mainwindow.stackWidget.setCurrentIndex(0) # Show mainwindow first
-    mainwindow.stackWidget.show() # Show the stackWidget
+    stackWidget.setCurrentIndex(0)  # Show mainwindow first
+    stackWidget.show()  # Show the stackWidget
     
-    sys.exit(mainwindow.app.exec_())
-window()                     
+    sys.exit(app.exec_())
+
+main()
