33282396d90f4705406a4610f97fcf6cfa0f302c
yolo1CLo <122444241+yolo1CLo@users.noreply.github.com>
2024-04-11 18:12:09 +0200

    Add files via upload

--- PyQT.py
+++ PyQT.py
@@ -5,7 +5,7 @@ import sys
 class MyWindow(QMainWindow):
     def __init__(self, stackWidget):
         super(MyWindow, self).__init__()
-        uic.loadUi('screen1.ui', self)  # Load the screen.ui file parameters
+        uic.loadUi('main_screen_0.ui', self)  # Load the screen.ui file parameters
         self.stackWidget = stackWidget
         self.pushButton.clicked.connect(self.gotoCalc)  # Assuming pushButton exists in screen1.ui
 
@@ -15,7 +15,7 @@ class MyWindow(QMainWindow):
 class Calc(QMainWindow):
     def __init__(self, stackWidget):
         super(Calc, self).__init__()
-        uic.loadUi('screen2.ui', self)
+        uic.loadUi('calc_screen_1.ui', self)
         self.stackWidget = stackWidget
         self.pushButton_1.clicked.connect(self.gotoQuadratic)  # Assuming pushButton_1 exists in screen2.ui
 
@@ -25,14 +25,18 @@ class Calc(QMainWindow):
 class Quadratic(QMainWindow):
     def __init__(self, stackWidget):
         super(Quadratic, self).__init__()
-        uic.loadUi('quadratic.ui', self)
+        uic.loadUi('quad_screen_2.ui', self)
         self.stackWidget = stackWidget
-        # Add any button connections here if needed
+        self.a_val = a_DSB.value()
+        self.b_val = b_DSB.value()
+        self.c_val = c_DSB.value()
+    def idk():
+        answer = a.val * b.val()
+        
 
 def main():
     app = QApplication(sys.argv) #QWidget: Must construct a QApplication before a QWidget
     stackWidget = QtWidgets.QStackedWidget()  # Centralized QStackedWidget
-
     
     # Create screen instances and pass the shared QStackedWidget to them
     main_window = MyWindow(stackWidget)
@@ -50,4 +54,3 @@ def main():
     sys.exit(app.exec_())
 
 main()
-#yolo 
